a0241fad4b8b22c0538dff50364cd430aaee5c67
Georg Schulz <git@ontilt.de>
2014-01-06 19:24:56 +0100

    mainly made the world clock toggable

--- xmonad/scripts/world_clock.sh
+++ xmonad/scripts/world_clock.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-
+DURATION=30
 FGCOL=green
 BGCOL=black
 TITLECOL=green4
 LOCATIONCOL=blue
 TIMECOL=grey
 #FONT='-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*'
-
 #FONT="Verdana-9"
 #FONT='-*-profont-*-*-*-*-11-*-*-*-*-*-iso8859'
 FONT="-*-roboto-medium-r-normal-*-14-*-*-*-*-*-*-1"
+LOCKFILE="/tmp/worldclock.sh_visible"
+
+pids=`pidof dzen2`
 local_location="Frankfurt"
 local_time=`date +"%H.%M"`
 #ger_location="Frankfurt"
@@ -23,28 +25,38 @@ aus_time=`TZ="Australia/Sydney" date +"%H:%M"`
 jp_location="Tokyo"
 jp_time=`TZ="Asia/Tokyo" date +"%H:%M"`
 
+if [ -e $LOCKFILE ]
+then
+    rm $LOCKFILE
+    kill `pgrep -f "dzen2.*worldclock"`
+    kill `pidof -o $$ -x "world_clock.sh"`
+else
+    touch $LOCKFILE
+    (
+    #echo -e "^fg($TITLECOL)World Time^fg() " 
+    echo
+    echo -e "^fg($LOCATIONCOL)$us_location^fg()"
+    echo -e "^fg($TIMECOL)$us_time^fg()"
+    echo
+    echo -e "^fg($LOCATIONCOL)$uk_location^fg()"
+    echo -e "^fg($TIMECOL)$uk_time ^fg()"
+    echo
+    echo -e "^fg($LOCATIONCOL)$local_location^fg()"
+    echo -e "^fg($TIMECOL)$local_time ^fg()"
+    echo
+    echo -e "^fg($LOCATIONCOL)$jp_location^fg()"
+    echo -e "^fg($TIMECOL)$jp_time^fg()"
+    echo
+    echo -e "^fg($LOCATIONCOL)$aus_location^fg()"
+    echo -e "^fg($TIMECOL)$aus_time^fg()"
 
-touch /tmp/worldclock.sh_visible
-(
-#echo -e "^fg($TITLECOL)World Time^fg() " 
-echo
-echo -e "^fg($LOCATIONCOL)$us_location^fg()"
-echo -e "^fg($TIMECOL)$us_time^fg()"
-echo
-echo -e "^fg($LOCATIONCOL)$uk_location^fg()"
-echo -e "^fg($TIMECOL)$uk_time ^fg()"
-echo
-echo -e "^fg($LOCATIONCOL)$local_location^fg()"
-echo -e "^fg($TIMECOL)$local_time ^fg()"
-echo
-echo -e "^fg($LOCATIONCOL)$jp_location^fg()"
-echo -e "^fg($TIMECOL)$jp_time^fg()"
-echo
-echo -e "^fg($LOCATIONCOL)$aus_location^fg()"
-echo -e "^fg($TIMECOL)$aus_time^fg()"
-
-echo -e "^uncollapse()"
-echo "^stick()"
-echo "^scrollhome()"
-) | dzen2 -fg $FGCOL -bg $BGCOL -x 35 -y 450 -xs 1 -l 15 -ta c -sa c -w 100 -p 10 -fn $FONT 
-rm /tmp/worldclock.sh_visible
+    echo -e "^uncollapse()"
+    echo "^stick()"
+    echo "^scrollhome()"
+    ) | dzen2 -fg $FGCOL -bg $BGCOL -x 35 -y 450 -xs 1 -l 15 -ta c -sa c -w 100 -p $DURATION -fn $FONT -title-name worldclockdzenmaster -slave-name worldclockdzenslave&
+    sleep 0.1
+    transset-df .9 -n worldclockdzenmaster 
+    transset-df .9 -n worldclockdzenslave
+    sleep `bc <<< $DURATION-0.1` 
+    rm $LOCKFILE
+fi
