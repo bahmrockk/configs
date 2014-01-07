c3169667f5af18e175326ed033dadd2daa60bb91
bahmrockk <bahmrockk@users.noreply.github.com>
2014-01-07 12:55:28 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -11,11 +11,25 @@ Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf'
 fi
 
 ### CHECKOUT IF USEFUL: trayer
-
+#xf86-video-nv or xf86-video-ati
 # Packages which are needed for what is configured here
-sudo pacman -S dzen2 xmonad xmonad-contrib zsh git slim pkgfile pkgfile lxrandr wget goldendict bc yaourt # -- needed
+printf "Do you use 1) nvidia card or 2) ati card? (any other card: install drivers manually beforehand! press any other key to quit [1|2|q]"
+read choice
+shopt -s nocasematch
+case "$choice" in
+    1) 
+    sudo pacman -S xf86-video-nvidia nvidia nvidia-utils
+    sudo modprobe nvidia
+    ;;
+    2) 
+    sudo pacman -S xf86-video-ati lib32-ati-dri
+    sudo modprobe radeon
+    *) exit
+    
+sudo pacman -S xorg-server xorg-xinit xorg-utils xorg-server-utils
+sudo pacman -S rxvt feh dzen2 xmonad xmonad-contrib zsh git slim pkgfile pkgfile lxrandr wget goldendict bc yaourt fakeroot # -- needed
 # Packages which I like or want in addition to the core
-sudo pacman -S firefox chromium thunderbird pidgin pidgin-otr playonlinux steam ntp flashplugin viewnior 
+sudo pacman -S firefox chromium thunderbird pidgin pidgin-otr ntp flashplugin viewnior 
 sudo chsh -s zsh bahmrockk
 
 #packages from the AUR world:
