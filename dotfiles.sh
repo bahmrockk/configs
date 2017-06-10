864292b50e533b48d114994ad082d6f0b714fb0e
bahmrockk <git@ontilt.de>
2017-06-10 14:42:17 +0200

    zsh update

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #set -e
-configurations=(conky i3 zsh)
-dotfiles=(xinitrc) 
+dotfiles=(zshenv)
 echo "You are about to remove the following configuration folders:"
-for i in ${configurations[@]}; do
+for i in ${dotfiles[@]}; do
     echo $i
 done
 read -p "Do you want to proceed? [yes|anything]
 " yn #linebreak for "read"
 case $yn in
     yes) 
-        for i in ${configurations[@]}; do
-            ln -sf $(pwd)/$i ~/.config/$i
+        for i in ${dotfiles[@]}; do
+            ln -sf $(pwd)/dotfiles/$i ~/.$i
         done
         ;; 
     *) echo "no";;
